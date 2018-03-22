fbe8b6d51ed3a4b5cec6d31114352fc5b701deaf
Petter Blomberg <petter@studi.se>
2018-03-22 17:49:45 +0100

    Put request result into CloudWatch metric
    This will create a CloudWatch metric called "status" in the namespace "cloudping", with the value 0 if the request succeeded or 1 if it failed. You can use this to create CloudWatch alarms that are specific for the URL, instead of monitoring if the lambda function failed or not.

--- cloudping.py
+++ cloudping.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
 import requests
+import json
+import boto3
+import datetime
 
 
 def ping(event, context):
@@ -14,9 +17,50 @@ def ping(event, context):
         'timeout': 5,
     }
     options.update(event)
-    response = requests.request(
-        options['method'],
-        '{protocol}://{domain}{path}'.format(**options),
-        allow_redirects=options['allow_redirects'],
-        timeout=options['timeout'])
-    response.raise_for_status()
+    url = '{protocol}://{domain}{path}'.format(**options)
+
+    try:
+        response = requests.request(
+            options['method'],
+            url,
+            allow_redirects=options['allow_redirects'],
+            timeout=options['timeout'])
+        response.raise_for_status()
+        result_value = 0
+    except Exception as e:
+        print(str(e))
+        result_value = 1
+
+    print(json.dumps({
+        'cloudping_result': result_value,
+        'url': url,
+        'options': options
+    }))
+
+    client = boto3.client('cloudwatch')
+    response = client.put_metric_data(
+        Namespace='cloudping',
+        MetricData=[
+            {
+                'MetricName': 'status',
+                'Dimensions': [
+                    {
+                        'Name': 'url',
+                        'Value': url
+                    },
+                    {
+                        'Name': 'method',
+                        'Value': options['method']
+                    },
+                    {
+                        'Name': 'protocol',
+                        'Value': options['protocol']
+                    },
+                ],
+                'Timestamp': datetime.datetime.utcnow(),
+                'Value': result_value,
+                'Unit': 'None',
+                'StorageResolution': 60
+            },
+        ]
+    )
